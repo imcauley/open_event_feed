832e11eb0b67b00fda9bc4f399e34ca88a992389
Isaac McAuley <mcauleyisaac@gmail.com>
2026-04-03 23:28:54 -0400

    json encoding of event item

--- src/open_event_feed/event_item.py
+++ src/open_event_feed/event_item.py
@@ -1,7 +1,14 @@
 from dataclasses import dataclass
 from typing import Optional, List, Dict
 import datetime
+import json
 
+class EventItemJSONEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.datetime)):
+            return obj.isoformat()
+
+        return json.JSONEncoder.default(self, obj)
 
 @dataclass
 class EventItem:
@@ -24,6 +31,12 @@ class EventItem:
     # needs start_datetime if end_datetime exists
     # needs organizer_link if organizer name exists
 
+    def toJSON(self):
+        return json.dumps(
+            self.__dict__,
+            cls=EventItemJSONEncoder,
+            sort_keys=True)
 
 if __name__ == "__main__":
-    e1 = EventItem(title="test", link="sdfklsdf")
+    e1 = EventItem(title="test", link="sdfklsdf", start_datetime=datetime.datetime.now(), additional_data={"test": "abc123"})
+    print(e1.toJSON())
