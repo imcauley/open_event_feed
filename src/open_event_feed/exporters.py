1f471b43658fd2d6542b11777bc002ecb07e56d7
Isaac McAuley <mcauleyisaac@gmail.com>
2026-05-12 19:17:39 -0400

    Writing xcal to file

--- src/open_event_feed/exporters.py
+++ src/open_event_feed/exporters.py
@@ -2,14 +2,14 @@ import datetime
 import json
 import xml.etree.ElementTree as ET
 
-class EventItemJSONEncoder(json.jsonEncoder):
+class EventItemJSONEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, (datetime.datetime, date)):
             return obj.isoformat()
 
         return json.JSONEncoder.default(self, obj)
 
-def xcal_from_event_items(event_item_list):
+def xcal_from_event_items(event_item_list, file):
     # From RFC https://www.rfc-editor.org/rfc/rfc6321.html
 
     root = ET.Element('icalendar')
@@ -21,5 +21,8 @@ def xcal_from_event_items(event_item_list):
     for event_item in event_item_list:
         components.append(event_item.to_xcal_vevent())
 
-    return root
+    tree = ET.ElementTree(root)
+    ET.indent(tree)
+
+    tree.write(file, encoding="UTF-8", xml_declaration=True)
 
